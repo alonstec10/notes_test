cf96e704b233b219a2dcfe77ce03c0a132e37bcd
Alan Fiedler <alonstec10@gmail.com>
2017-08-14 16:13:14 -0700

    AJF: updated notes controller to update, delete and get note records

--- application/controllers/NotesController.php
+++ application/controllers/NotesController.php
@@ -33,43 +33,92 @@ class NotesController extends CI_Controller {
 		$this->output->set_content_type('application/json')->set_output(json_encode($response));
 	}
 
+	//create a response object
 	public function insertNote()
 	{
 		$response = [];
-		$response['method'] = 'INSERT';
-
-
+		
 		//public function createNote( $note_title, $note )
 		$user_id = $this->input->post('user_id');
 		$note_title = $this->input->post('title');
 		$note = $this->input->post('note');
 
-		if( empty($user_id) || empty($note_title) || !$this->NoteModel->createNote($note_title, $note))
+		if( empty($user_id) || empty($note_title))
 		{
 			//throw some exception 
 			//throw status 500
-			echo "there as an error";
+			//echo "there as an error"
+			$this->output->set_status_header(500);
 			exit;
+		} 
+		else if (!$this->NoteModel->createNote($note_title, $note)) {
+			$this->output->set_status_header(403);
+			exit;
+		} else 
+		{
+			$response['success'] = true;
+			$this->output->set_header('HTTP/1.0 201 OK');	
+			$this->output->set_content_type('application/json')->set_output(json_encode($response));
 		}
-
-
-
-		$this->output->set_header('HTTP/1.0 201 OK');	
-		$this->output->set_content_type('application/json')->set_output(json_encode($response));
 	}
 
 	public function updateNote()
 	{
 		$response = [];
-		$response['method'] = 'UPDATE';
-		$this->output->set_content_type('application/json')->set_output(json_encode($response));
+		//$response['method'] = 'UPDATE';
+
+		$user_id = $this->input->input_stream('user_id');
+		$note_id = $this->input->input_stream('note_id');
+		$note = $this->input->input_stream('note');
+		$note_title = $this->input->input_stream('note');
+
+
+		if( empty( $user_id) || empty( $note))
+		{
+			//throw some exception 
+			//throw status 500
+			//echo "there as an error"
+			$this->output->set_status_header(500);
+			exit;
+		} 
+		else if (!$this->NoteModel->updateNoteById($note_id, $note_title, $note))
+		{
+			$this->output->set_status_header(403);
+			exit;
+		}
+		else
+		{
+			$response['success'] = true;
+			$this->output->set_content_type('application/json')->set_output(json_encode($response));
+		}
 	}
 
 	public function deleteNote()
 	{
 		$response = [];
-		$response['method'] = 'DELETE';
-		$this->output->set_content_type('application/json')->set_output(json_encode($response));
+		//$response['method'] = 'DELETE';
+
+		$user_id = $this->input->input_stream('user_id');
+		$note_id = $this->input->input_stream('note_id');
+
+		if( empty( $user_id) || empty($note_id)) 
+		{
+			//throw some exception 
+			//throw status 500
+			//echo "there as an error"
+			$this->output->set_status_header(500);
+			exit;
+		} 
+		else if( !$this->NoteModel->deleteNoteById($note_id) )
+		{
+			$this->output->set_status_header(403);
+			exit;
+		}
+		else
+		{
+			$response['success'] = true;
+			$this->output->set_content_type('application/json')->set_output(json_encode($response));
+		}
 	}
 
 
